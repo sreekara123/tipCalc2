89f309db290110a17fd82720cdc24b026f0e7b33
Sreekara Yachamaneni <syachama@purdue.edu>
2020-01-20 20:25:22 -0500

    Initial Commit

---
+++ tipCalc2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc2
+//
+//  Created by Sreekara Yachamaneni on 1/20/20.
+//  Copyright © 2020 Sreekara Yachamaneni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
